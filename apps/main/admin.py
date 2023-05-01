01e0b575dd1bb425de437456f3254bc8a42648c0
Aji Kumar TechArtist <ajiartist2003@gmail.com>
2023-05-01 10:14:29 +0530

    files upload

---
+++ apps/main/admin.py
@@ -0,0 +1,129 @@
+from django.contrib import admin
+from django.shortcuts import get_object_or_404
+from .models import *
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import Widget
+from datetime import datetime as date_
+
+admin.site.site_header = 'Ponjesly Administration'
+
+
+class StaffResource(resources.ModelResource):
+    class Meta:
+        model = Staff
+
+
+class StaffAdmin(ImportExportModelAdmin):
+    resource_classes = [StaffResource]
+
+
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = Student
+
+
+class StudentAdmin(ImportExportModelAdmin):
+    resource_classes = [StudentResource]
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_filter = ['department', 'year', 'semester']
+    list_display = ['subject_code', 'subject_name',
+                    'staff', 'department', 'year', 'semester']
+
+
+class TimeTableAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/timetable_change_list.html'
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        values = False
+        if request.method == 'POST':
+            dept = request.POST.get('department')
+            year = request.POST.get('year')
+            date = request.POST.get('date')
+            values = True
+            if dept == 'IT':
+                dept = 'Information Technology'
+            if dept == 'CSE':
+                dept = 'Computer Science and Engineering'
+            elif dept == 'ECE':
+                dept = 'Electronics and Communication Engineering'
+            elif dept == 'EEE':
+                dept = 'Electrical and Electronic Engineering'
+            elif dept == 'Mech':
+                dept = 'Mechanical Engineering'
+            elif dept == 'Civil':
+                dept = 'Civil Engineering'
+            timetable_data = TimeTable.objects.filter(
+                period_1__department=dept, period_1__year=year, date=date)
+            sub = Subject.objects.filter(department=dept, year=year)
+            extra_context.update({
+                'timetable': timetable_data,
+                'values': values,
+                'sub': sub
+            })
+
+            return super().changelist_view(request, extra_context=extra_context)
+        else:
+            return super().changelist_view(request, extra_context=extra_context)
+
+
+class StudentAttendanceAdmin(admin.ModelAdmin):
+    add_form_template = 'admin/attend.html'
+    list_filter = ['name__Department', 'name__year', 'date']
+
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        values = False
+        if request.method == 'POST':
+            dept = request.POST.get('department')
+            year = request.POST.get('year')
+            date = request.POST.get('date')
+            values = True
+            if dept == 'IT':
+                dept = 'Information Technology'
+            if dept == 'CSE':
+                dept = 'Computer Science and Engineering'
+            elif dept == 'ECE':
+                dept = 'Electronics and Communication Engineering'
+            elif dept == 'EEE':
+                dept = 'Electrical and Electronic Engineering'
+            elif dept == 'Mech':
+                dept = 'Mechanical Engineering'
+            elif dept == 'Civil':
+                dept = 'Civil Engineering'
+            day = date_.today().strftime("%A")
+
+            sub = Subject.objects.filter(department=dept, year=year)
+            student = Student.objects.filter(Department=dept, year=year)
+            tt = TimeTable.objects.filter(
+                department=dept, year=year, date=date, day='Monday')
+            subname = []
+            for i in tt:
+                subname.append(i.period_1.subject_name)
+                subname.append(i.period_2.subject_name)
+                subname.append(i.period_3.subject_name)
+                subname.append(i.period_4.subject_name)
+                subname.append(i.period_5.subject_name)
+                subname.append(i.period_6.subject_name)
+                subname.append(i.period_7.subject_name)
+                
+            extra_context.update({
+                "sub": sub,
+                "student": student,
+                'values': values,
+                'subname': subname
+            })
+
+            return self.changeform_view(request, None, form_url, extra_context)
+        else:
+            return self.changeform_view(request, None, form_url, extra_context)
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(TimeTable, TimeTableAdmin)
+admin.site.register(StudentAttendance, StudentAttendanceAdmin)
---
+++ apps/main/models.py
@@ -0,0 +1,290 @@
+from django.db import models
+import datetime
+
+year_choices = [
+    ('First', 'First'),
+    ('Second', 'Second'),
+    ('Third', 'Third'),
+    ('Fourth', 'Fourth'),
+]
+
+dept_choices = [
+    ('Information Technology', 'IT'),
+    ('Computer Science and Engineering', 'CSE'),
+    ('Electronics and Communication Engineering', 'ECE'),
+    ('Electrical and Electronic Engineering', 'EEE'),
+    ('Mechanical Engineering', 'Mech'),
+    ('Civil Engineering', 'Civil')
+]
+gender_choices = [
+    ('Male', 'male'),
+    ('Female', 'female')
+]
+
+category_choices = [
+    ('OC', 'OC'),
+    ('BC', 'BC'),
+    ('MBC', 'MBC'),
+    ('SC\ST', 'SC\ST'),
+    ('Other', 'Other')
+]
+guardian_choice = [
+    ('Father', 'Father'),
+    ('Mother', 'Mother'),
+    ('Local Guardian', 'Local Guardian'),
+    ('Local Guardian 2', 'Local Guardian 2')
+]
+transport_choice = [
+    ('College bus', 'College bus'),
+    ('Own vehicle', 'Own vehicle'),
+    ('By walk', 'By walk')
+]
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    Admission_number = models.IntegerField(null=True, blank=True)
+    Admission_date = models.DateField(null=True, blank=True)
+    Batch_name = models.CharField(max_length=20, null=True, blank=True)
+    Register = models.IntegerField(null=True, blank=True)
+    Roll_no = models.IntegerField(null=True, blank=True)
+    Age = models.IntegerField(null=True)
+    DOB = models.DateField(null=True, blank=True)
+    Gender = models.CharField(max_length=10, choices=gender_choices, null=True)
+    Blood_group = models.CharField(max_length=5, null=True, blank=True)
+    Department = models.CharField(
+        max_length=200, choices=dept_choices, null=True)
+    year = models.CharField(
+        max_length=200, default='First', choices=year_choices, null=True)
+    Phone = models.IntegerField(null=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    Aadhaar_no = models.IntegerField(null=True, blank=True)
+    Father_name = models.CharField(max_length=200, blank=True)
+    Father_phone = models.IntegerField(null=True, blank=True)
+    Occupation = models.CharField(max_length=50, null=True, blank=True)
+    Mother_name = models.CharField(max_length=200, blank=True)
+    Mother_phone = models.IntegerField(null=True, blank=True)
+    Mother_Occupation = models.CharField(max_length=50, null=True, blank=True)
+    Annual_income = models.IntegerField(null=True, blank=True)
+    Nationality = models.CharField(max_length=10, null=True, blank=True)
+    Religion = models.CharField(max_length=30, null=True, blank=True)
+    Student_category = models.CharField(
+        max_length=10, choices=category_choices, null=True, blank=True)
+    Door_no = models.IntegerField(null=True, blank=True)
+    Street_and_Area = models.CharField(max_length=200, null=True, blank=True)
+    District = models.CharField(max_length=50, null=True, blank=True)
+    State = models.CharField(max_length=50, null=True, blank=True)
+    Country = models.CharField(max_length=50, null=True, blank=True)
+    Pincode = models.IntegerField(null=True, blank=True)
+    Mode_of_Transpotation = models.CharField(
+        max_length=50, choices=transport_choice, null=True, blank=True)
+    Bus_route_no = models.IntegerField(null=True, blank=True)
+    Rugular_Boarding_point = models.CharField(
+        max_length=50, null=True, blank=True)
+    Regular_dropping_point = models.CharField(
+        max_length=50, null=True, blank=True)
+    Guardian_name = models.CharField(max_length=50, null=True, blank=True)
+    Guardian_is = models.CharField(
+        max_length=20, choices=guardian_choice, null=True, blank=True)
+    Guardian_mobile = models.IntegerField(null=True, blank=True)
+    Guardian_Address = models.CharField(max_length=300, null=True, blank=True)
+    Guardian_2_name = models.CharField(max_length=50, null=True, blank=True)
+    Guardian_2_is = models.CharField(
+        max_length=20, choices=guardian_choice, null=True, blank=True)
+    Guardian_2_mobile = models.IntegerField(null=True, blank=True)
+    Guardian_2_Address = models.CharField(
+        max_length=300, null=True, blank=True)
+    Emergency_contact = models.CharField(
+        max_length=30, choices=guardian_choice, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+employee_choice = [
+    ('Assistant professor', 'Assistant professor'),
+    ('Professor', 'Professor'),
+    ('Lab Staff', 'Lab Staff'),
+    ('Supporting Staff', 'Supporting Staff'),
+    ('Driver', 'Driver'),
+    ('Maintenance Staff', 'Maintenance Staff')
+]
+marital_choice = [
+    ('Single', 'Single'),
+    ('Married', 'Married')
+]
+Emergency_choice = [
+    ('Father', 'Father'),
+    ('Mother', 'Mother'),
+    ('Spouse', 'Spouse')
+]
+
+
+class Staff(models.Model):
+    Employee_ID = models.CharField(max_length=5, null=True, blank=True)
+    Employee_Category = models.CharField(
+        max_length=30, choices=employee_choice, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    role = models.CharField(max_length=200, null=True)
+    Father_name = models.CharField(max_length=50, null=True, blank=True)
+    Mother_name = models.CharField(max_length=50, null=True, blank=True)
+    phone = models.IntegerField(null=True)
+    email = models.CharField(max_length=200, blank=True)
+    Home_contact_No = models.IntegerField(null=True, blank=True)
+    Age = models.IntegerField(null=True)
+    Gender = models.CharField(max_length=10, choices=gender_choices, null=True)
+    Blood_group = models.CharField(max_length=10, null=True, blank=True)
+    Aadhaar_no = models.IntegerField(null=True, blank=True)
+    DOB = models.DateField(null=True, blank=True)
+    Date_of_Joining = models.DateField(null=True, blank=True)
+    Marital_Status = models.CharField(
+        max_length=10, choices=marital_choice, null=True, blank=True)
+    Spouse_Name = models.CharField(max_length=30, null=True, blank=True)
+    Children_count = models.IntegerField(null=True, blank=True)
+    Department = models.CharField(max_length=200, choices=dept_choices)
+    Qualification = models.CharField(max_length=50, null=True, blank=True)
+    Experience = models.CharField(max_length=50, null=True, blank=True)
+    Religion = models.CharField(max_length=30, null=True, blank=True)
+    Community = models.CharField(
+        max_length=10, choices=category_choices, null=True, blank=True)
+    Door_no = models.IntegerField(null=True, blank=True)
+    Street_and_Area = models.CharField(max_length=200, null=True, blank=True)
+    District = models.CharField(max_length=50, null=True, blank=True)
+    State = models.CharField(max_length=50, null=True, blank=True)
+    Country = models.CharField(max_length=50, null=True, blank=True)
+    Pincode = models.IntegerField(null=True, blank=True)
+    Office_Door_no = models.IntegerField(null=True, blank=True)
+    Office_Street_and_Area = models.CharField(
+        max_length=200, null=True, blank=True)
+    Office_District = models.CharField(max_length=50, null=True, blank=True)
+    Office_State = models.CharField(max_length=50, null=True, blank=True)
+    Office_Country = models.CharField(max_length=50, null=True, blank=True)
+    Office_Pincode = models.IntegerField(null=True, blank=True)
+    Mode_of_Transpotation = models.CharField(
+        max_length=50, choices=transport_choice, null=True, blank=True)
+    Emergency_contact = models.CharField(
+        max_length=30, choices=Emergency_choice, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+regulation_choices = [
+    ('2017 regulation', '2017 regulation'),
+    ('2021 regulation', '2021 regulation')
+]
+
+sem_choices = [
+    ('First', 'First'),
+    ('Second', 'Second'),
+]
+
+year_choices = [
+    ('First', 'First'),
+    ('Second', 'Second'),
+    ('Third', 'Third'),
+    ('Fourth', 'Fourth'),
+]
+
+
+day_choice = [
+    ("Monday", "Monday"),
+    ("Tuesday", "Tuesday"),
+    ("Wednesday", "Wednesday"),
+    ("Thursday", "Thursday"),
+    ("Friday", "Friday")
+]
+
+period_choice = [
+    ('First', 'First'),
+    ('Second', 'Second'),
+    ('Third', 'Third'),
+    ('Fourth', 'Fourth'),
+    ('Fifth', 'Fifth'),
+    ('Sixth', 'Sixth'),
+    ('Seventh', 'Seventh'),
+    ('Eight', 'Eight'),
+
+]
+
+
+class Subject(models.Model):
+    year_choices = [('First', 'First'), ('Second', 'Second'),
+                    ('Third', 'Third'), ('Fourth', 'Fourth')]
+    sem_choices = [('odd', 'Odd'), ('even', 'Even')]
+    subject_code = models.CharField(max_length=100, null=True)
+    subject_name = models.CharField(max_length=50, null=True)
+    regulation = models.CharField(
+        max_length=100, null=True, choices=regulation_choices)
+    department = models.CharField(
+        choices=dept_choices, max_length=100, null=True, default='CSE')
+    year = models.CharField(max_length=100, choices=year_choices)
+    semester = models.CharField(max_length=20, choices=sem_choices)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, default="")
+
+    def __str__(self):
+        return str(self.subject_name)
+
+
+class TimeTable(models.Model):
+    day = models.CharField(max_length=50, null=True, choices=day_choice)
+    regulation = models.CharField(
+        max_length=100, null=True, choices=regulation_choices)
+    department = models.CharField(
+        choices=dept_choices, max_length=100, null=True, default='CSE')
+    year = models.CharField(max_length=100, null=True, choices=year_choices)
+    semester = models.CharField(max_length=20, null=True, choices=sem_choices)
+    date = models.DateField(default=datetime.datetime.now)
+    period_1 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_1', null=True)
+    period_2 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_2', null=True)
+    period_3 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_3', null=True)
+    period_4 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_4', null=True)
+    period_5 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_5', null=True)
+    period_6 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_6', null=True)
+    period_7 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_7', null=True)
+    period_8 = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name='period_8', null=True)
+
+    def __str__(self):
+        return f'{self.period_1.year} {self.period_1.department} {self.day}'
+
+
+att_choice = [
+    ('Present', 'Present'),
+    ('Absent', 'Absent'),
+    ('On Duty', 'On Duty')
+]
+
+
+class StudentAttendance(models.Model):
+    name = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField()
+    period_1 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p1reason = models.CharField(max_length=500, null=True, blank=True)
+    period_2 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p2reason = models.CharField(max_length=500, null=True, blank=True)
+    period_3 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p3reason = models.CharField(max_length=500, null=True, blank=True)
+    period_4 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p4reason = models.CharField(max_length=500, null=True, blank=True)
+    period_5 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p5reason = models.CharField(max_length=500, null=True, blank=True)
+    period_6 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p6reason = models.CharField(max_length=500, null=True, blank=True)
+    period_7 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p7reason = models.CharField(max_length=500, null=True, blank=True)
+    period_8 = models.CharField(max_length=100, choices=att_choice, null=True)
+    p8reason = models.CharField(max_length=500, null=True, blank=True)
+    reason = models.CharField(max_length=500, null=True, blank=True)
+    mark_attendance = models.CharField(
+        max_length=100, choices=att_choice, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
